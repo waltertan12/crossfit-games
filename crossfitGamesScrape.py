567e4689d6e9afaab8f52bb9b16971017d80e195
Walter Tan <waltertan12@gmail.com>
2015-05-10 16:50:03 -0400

    Clean / organize data

--- crossfitGamesScrape.py
+++ crossfitGamesScrape.py
@@ -1,99 +1,205 @@
-import requests, bs4, openpyxl, re, sqlite3
+import requests, bs4, re, sqlite3
 
-baseURL = 'http://games.crossfit.com/athlete/'
-wb = openpyxl.load_workbook('cf1.xlsx',data_only=True)
-sheet = wb.get_sheet_by_name('Sheet1')
-
-excelIndex = int(sheet['B1'].value) + 1
-excelStart = int(sheet['A' + str(excelIndex -1 )].value)
-
-col = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q']
-
-for x in range(excelStart+1,700000):
-	# Download page
-	res = requests.get(baseURL + str(x))
-	res.raise_for_status()
-	soup = bs4.BeautifulSoup(res.text)
-
-	# Regular expression to parse text from <dd></dd>
-	regDD = re.compile(r'<dd>(.*)</dd>')
-
-	# Regular expression to parse text from <td></td>
-	regTD = re.compile(r'<td>(.*)</td>')
-
-	# Continue only if athlete is found
-	if str(soup.select('#page-title')).find(r'Not found') < 0:
-		print("Scraping " + baseURL + str(x))
-		last = len(soup.select('dd'))
-
-		# Fill ID number
-		sheet['A' + str(excelIndex)] = x
-
-		# Fill Gender
-		if str(soup.select('dd')).find('Female') < 0:
-			sheet['B' + str(excelIndex)] = 'Female'
-
-		else:
-			sheet['B' + str(excelIndex)] = 'Male'
-
-		# Fill age
-		# sheet['C' + str(excelIndex)] = int(str(soup.select('dd')[last-3])[4:len(str(soup.select('dd')[last-3])[4:])-1])
-
-		# Fill height
-		# sheet['D' + str(excelIndex)] = str(soup.select('dd')[last-2])[4:len(str(soup.select('dd')[last-2])[4:])-1]
-
-		# Fill weight
-		weight = regDD.search(str(soup.select('dd')[last-1]))
-		if weight.find("lb") > 0:
-			# Weight is already in pounds, insert to db
-			sheet('E' + str(excelIndex)) = int(weight[:len(weight)-3])
-		else:
-			# Convert weight from kilograms to pounds
-			sheet('E' + str(excelIndex)) = int(weight[:len(weight)-3]) * 2.204
-		# sheet['E' + str(excelIndex)] = str(soup.select('dd')[last-1])[4:len(str(soup.select('dd')[last-1])[4:])-1]
-
-		# Fill workout data
-		i = 1
-		c = 5
-		while i <= 23:
-			sheet[col[c] + str(excelIndex)] = str(soup.select('td')[i])[4:len(str(soup.select('td')[i])[4:])-1]
-			c += 1
-			i += 2
-
-
-		# Next row on Excel sheet
-		excelIndex += 1
-
-	else:
-		print('Athlete ' + str(x) + ' not found')
-
-	if x%25 == 0:
-			wb.save('cf.xlsx')
-
+# Parse individual stats
 def parseRank():
 	# Ranking URL
 	url1 = "http://games.crossfit.com/scores/leaderboard.php?stage=0&sort=0&page="
 	url2 = "&division=1&region=0&numberperpage=100&competition=0&frontpage=0&expanded=0&year=15&full=0&showtoggles=1&hidedropdowns=1&showathleteac=0&=&is_mobile=&scaled=0&fittest=1&fitSelect=0"
 	pageNumber = 1
 
-	# Full URL
-	url = url1 + str(pageNumber) + url2
+	# Regular expressions to parse data
+	reg1 = re.compile(">(.*) \(")
+	reg2 = re.compile('a href="(.*)" target')
+	reg3 = re.compile('\((.*)\)')
 
-	print(url)
+	# Index
+	index = 0
 
+	while pageNumber <= 1000:
+		# Full URL
+		url = url1 + str(pageNumber) + url2
+		print(url)
 
+		# Download rank page
+		res = requests.get(url)
+		res.raise_for_status()
+		soup = bs4.BeautifulSoup(res.text)
+		info = soup.select('tr > td')
 
-def parseAthleteStats(athlete):
-	# Athlete URL
-	url = 'http://games.crossfit.com/athlete/' + str(athlete)
+		# Parse page information
+		while index < len(info):
+			# Parse overall athlete rank
+			overallRank = int(reg1.search(str(info[index  ])).group(1))
 
+			# Parse athlete url
+			athleteURL 	= reg2.search(str(info[index+1])).group(1)
+
+			# Event 1
+			event1rank 	= int(reg1.search(str(info[index+2])).group(1))
+			event1score	= int(reg3.search(str(info[index+2])).group(1))
+
+			# Event 2
+			event2rank 	= int(reg1.search(str(info[index+3])).group(1))
+			event2score = int(reg3.search(str(info[index+3])).group(1))
+
+			# Event 3
+			event3rank 	= int(reg1.search(str(info[index+4])).group(1))
+			event3score = int(reg3.search(str(info[index+4])).group(1))
+
+			# Event 4
+			event4rank 	= int(reg1.search(str(info[index+5])).group(1))
+			event4score = int(reg3.search(str(info[index+5])).group(1))
+
+			# Event 5
+			event5rank 	= int(reg1.search(str(info[index+6])).group(1))
+			event5score = int(reg3.search(str(info[index+6])).group(1))
+
+			# Event 6
+			event6rank 	= int(reg1.search(str(info[index+7])).group(1))
+			event6score = seconds(str(reg3.search(str(info[index+7])).group(1)))
+
+			# Parse athlete
+			parseAthleteStats(athleteURL)
+
+			print("Rank: " + str(overallRank))
+			print("Event 1: Rank "+str(event1rank) 	+ ", " + str(event1score))
+			print("Event 2: Rank "+str(event2rank) 	+ ", " + str(event2score))
+			print("Event 3: Rank "+str(event3rank) 	+ ", " + str(event3score))
+			print("Event 4: Rank "+str(event4rank) 	+ ", " + str(event4score))
+			print("Event 5: Rank "+str(event5rank) 	+ ", " + str(event5score))
+			print("Event 6: Rank "+str(event6rank) 	+ ", " + str(event6score))
+
+			# Iterate through athletes
+			index += 8
+
+		# Iterate
+		pageNumber += 1
+		index = 0
+
+# 
+def parseAthleteStats(athleteURL):
 	# Download individual page
-	res = requests.get(baseURL + str(x))
+	res = requests.get(athleteURL)
 	res.raise_for_status()
 	soup = bs4.BeautifulSoup(res.text)
+	dd = soup.select('dd')
+	td = soup.select('td')
+	lastDD = len(dd)
+
+	reg1 = re.compile('http://games.crossfit.com/athlete/(.*)')
 
 	# Regular expression to parse text from <dd></dd>
 	regDD = re.compile(r'<dd>(.*)</dd>')
 
 	# Regular expression to parse text from <td></td>
 	regTD = re.compile(r'<td>(.*)</td>')
+
+	# Fill Gender
+	if str(dd).find('Female') >= 0:
+		gender = 'Female'
+	else:
+		gender = 'Male'
+
+	# Fill age
+	age 	= number(str(regDD.search(str(dd[lastDD-3])).group(1)))
+
+	# Fill height
+	height 	= inches(str(regDD.search(str(dd[lastDD-2])).group(1)))
+
+	# Fill weight
+	weight 	= lbkg(str(regDD.search(str(dd[lastDD-1])).group(1)))
+
+	# Fran
+	fran 	= seconds(str(regTD.search(str(td[ 1])).group(1)))
+
+	# Helen
+	helen	= seconds(str(regTD.search(str(td[ 3])).group(1)))
+
+	# Grace
+	grace	= seconds(str(regTD.search(str(td[ 5])).group(1)))
+
+	# Filthy 50
+	f50		= seconds(str(regTD.search(str(td[ 7])).group(1)))
+
+	# Fight Gone Bad
+	fgb		= number(str(regTD.search(str(td[ 9])).group(1)))
+
+	# 400m Sprint
+	fourH	= seconds(str(regTD.search(str(td[11])).group(1)))
+
+	# 5k Run
+	fiveK	= seconds(str(regTD.search(str(td[13])).group(1)))
+
+	# Clean and Jerk
+	cnj		= lbkg(str(regTD.search(str(td[15])).group(1)))
+
+	# Snatch
+	sn		= lbkg(str(regTD.search(str(td[17])).group(1)))
+
+	# Deadlift
+	dl 		= lbkg(str(regTD.search(str(td[19])).group(1)))
+
+	# Back Squat
+	bs 		= lbkg(str(regTD.search(str(td[21])).group(1)))
+
+	# Max Pullups
+	pu 		= number(str(regTD.search(str(td[23])).group(1)))
+
+	print("//////////////////////////////////////////////")
+	print("Athlete Number: " + reg1.search(athleteURL).group(1))
+	print("Gender: " + gender + " | Age: " + str(age) + " | Height: " + str(height) + " in | Weight: " + str(weight) + " lbs")
+
+	print("Fran: " + str(fran))
+	print("Helen: " + str(helen))
+	print("Grace: " + str(grace))
+	print("Filthy 50: " + str(f50))
+	print("Fight Gone Bad: " + str(fgb))
+	print("400m Sprint: " + str(fourH))
+	print("5k: " + str(fiveK))
+
+	print("Clean & Jerk: " + str(cnj))
+	print("Snatch: " + str(sn))
+	print("Deadlift: " + str(dl))
+	print("Back Squat: " + str(bs))
+	print("Max Pullups: " + str(pu))
+
+# Subfunction to convert clean weight data
+def lbkg(string):
+	if string.find("lb") >= 0:
+		# Weight is already in pounds, insert to db
+		return int(string[:len(string)-3])
+	elif string.find("kg") >= 0:
+		# Convert weight from kilograms to pounds
+		return int(string[:len(string)-3]) * 2.204
+	else:
+		return 0
+
+# Subfunction to clean time data
+def seconds(string):
+	if string.find("--") >= 0:
+		return 0
+	else:
+		reg = re.compile("(.*):(.*)")
+		minutes = int(reg.search(string).group(1))
+		seconds = int(reg.search(string).group(2))
+		return (60*minutes) + seconds
+
+# Subfunction to clean number data
+def number(string):
+	if string.find("--") >= 0:
+		return 0
+	else:
+		return int(string)
+
+# Subfunction to clean height
+def inches(string):
+	if string.find("--") >= 0:
+		return 0
+	elif string.find("cm") >= 0:
+		return int(string[:len(string)-3]) / 2.54
+	else:
+		string = string[:len(string)-1]
+		height = re.compile("(.*)'(.*)").search(string)
+		return (int(str(height.group(1))) * 12) + int(str(height.group(2)))
+
+parseRank()
