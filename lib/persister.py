4c00545d327925e2fab06c1406680508be414329
Walter Tan <wtan@coursehero.com>
2016-12-04 10:20:46 -0800

    Abstract insert and select into separate methods

--- lib/persister.py
+++ lib/persister.py
@@ -5,16 +5,46 @@ class Persister():
     def __init__(self, sqlite_file = "/../db/crossfit_open_data.db"):
         self.db_path = os.path.dirname(os.path.realpath(__file__)) + sqlite_file
 
-    def persist_athlete(self, athlete):
-        return True
+    def get_athlete_ids(self, starting_id = 0):
+        data = self.select("SELECT DISTINCT(athlete_id) FROM event WHERE athlete_id > " + str(starting_id) + ";")
+        athlete_ids = []
 
-    def persist_events(self, data):
+        for tup in data:
+            athlete_ids.append(tup[0])
+
+        return athlete_ids
+
+    def persist_athlete(self, athlete):
+        insert_query = self.get_athlete_insert_query(athlete)
+        try:
+            self.insert(insert_query)
+        except(Exception):
+            print(insert_query)
+
+    def persist_events(self, event_data):
+        insert_query = self.get_events_insert_query(event_data)
+        self.insert(insert_query)
+
+    def select(self, query):
         connection = sqlite3.connect(self.db_path)
         cursor = connection.cursor()
-        insert_query = self.get_events_insert_query(data)
-        cursor.execute(insert_query)
+        data = cursor.execute(query).fetchall()
         connection.commit()
         connection.close()
+
+        return data
+
+    def insert(self, query):
+        connection = sqlite3.connect(self.db_path)
+        cursor = connection.cursor()
+        cursor.execute(query)
+        connection.commit()
+        connection.close()
+
+    def get_athlete_insert_query(self, athlete):
+        insert_query = "REPLACE INTO athlete(athlete_id, stat, value) VALUES "
+        insert_query += athlete.get_stats_insert_values()
+        return insert_query[:-1] + ";"
 
     def get_events_insert_query(self, data):
         insert_query = '''
