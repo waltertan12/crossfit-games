ef2bf3a35399c38034b6521163f7e48eb128d76c
Walter Tan <wtan@coursehero.com>
2016-12-04 09:59:59 -0800

    Add new athlete table to db

--- db/schema.sql
+++ db/schema.sql
@@ -8,3 +8,11 @@ CREATE TABLE IF NOT EXISTS event(
 );
 
 CREATE UNIQUE INDEX athlete_id_event_id_index ON event (athlete_id, event_id);
+
+CREATE TABLE IF NOT EXISTS athlete(
+    athlete_id INTEGER NOT NULL,
+    stat STRING,
+    value STRING
+);
+
+CREATE UNIQUE INDEX athlete_id_stat_index ON athlete(athlete_id, stat);
